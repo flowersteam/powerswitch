dbfdc4163ef8792e9bfac183bb77868784361e94
Fabien Benureau <fabien.benureau+git@gmail.com>
2014-02-20 15:48:24 +0100

    10/10 on pylint

--- powerswitch/__init__.py
+++ powerswitch/__init__.py
@@ -1,1 +1,7 @@
+"""
+Typical usage:
+    import powerswitch
+    ps = powerswitch.Eps4m(mac_address='00:13:f6:01:52:d6', ip_address='193.50.110.135')
+    ps.set_all_on()
+"""
 from .powerswitch import Eps4m
